3cfef68ca8506967423526511ec982ff91fec421
Michal <aeladrir@gmail.com>
2023-01-04 09:37:39 +0100

    added variables in JS

---
+++ main.js
@@ -0,0 +1,13 @@
+const addBtn = document.querySelector('.add')
+const deleteAllBtn = document.querySelector('.delete-all')
+const deleteNote = document.querySelector('.delete-note')
+const saveBtn = document.querySelector('.save')
+const cancelBtn = document.querySelector('.cancel')
+
+const notePanel = document.querySelector('.note-panel')
+const categorySelect = document.querySelector('.category')
+const textArea = document.querySelector('.text')
+const errorText = document.querySelector('.error')
+
+const noteTitle = document.querySelector('.note-title')
+const note = document.querySelector('.note-body')
